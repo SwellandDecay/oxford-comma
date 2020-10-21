f3c6cc5811258071f8306d1441305147dc58ba2a
Garrett <Garrett.Bodley@gmail.com>
2020-10-21 14:45:36 -0400

    commit tree 7f5c44e5ec042114175fad2bbc421fc7a28ec5be

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
 
+    # Check array Length
+    if array.count <= 2
+        string = array.join(" and ")
+        
+    else
+        # If array has > 2 items, return a string of those items, separated by commas with an ", and " between the last two items
+        string = array[0..-2].join(", ")
+        string << ", and #{array[-1]}"
+        string
+    end
 end
